4a7356d993e071b6e73b184fad2478f42799b913
QU Jiashu <470486480@qq.com>
2018-05-30 18:27:00 +0800

    1.  邀请开发中。

---
+++ app/src/main/java/com/sujin/sjpay/activity/GetVipActivity.java
@@ -0,0 +1,19 @@
+package com.sujin.sjpay.activity;
+
+import android.os.Bundle;
+import com.sujin.sjpay.R;
+
+public class GetVipActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_vip);
+        initView();
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
