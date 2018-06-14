4134077990c97381d435498c137a1774931eca9c
zhaofan <294903742@qq.com>
2018-06-14 10:56:33 +0800

    回弹viewpage

--- app/src/main/java/com/sujin/sjpay/activity/InviteActivity.java
+++ app/src/main/java/com/sujin/sjpay/activity/InviteActivity.java
@@ -2,14 +2,14 @@ package com.sujin.sjpay.activity;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.util.Log;
+import android.support.v4.view.PagerAdapter;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.sohu.sdk.common.toolbox.LogUtils;
-import com.lidroid.xutils.db.table.KeyValue;
 import com.scwang.smartrefresh.header.MaterialHeader;
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
@@ -27,11 +27,9 @@ import com.sujin.sjpay.protocol.ShareResponse;
 import com.sujin.sjpay.util.DialogUtil;
 import com.sujin.sjpay.util.StringUtil;
 import com.sujin.sjpay.util.ToastUtil;
-import com.sujin.sjpay.view.ScrollzfView;
+import com.sujin.sjpay.view.MyListView;
 import com.sujin.sjpay.view.TitleBarView;
-import com.sujin.sjpay.view.dialog.BaseDialog;
 import com.sujin.sjpay.view.dialog.InviteDetailDialog;
-import com.sujin.sjpay.view.dialog.WheelViewDialog;
 import com.umeng.socialize.ShareAction;
 import com.umeng.socialize.UMShareListener;
 import com.umeng.socialize.bean.SHARE_MEDIA;
@@ -41,6 +39,7 @@ import com.yanzhenjie.nohttp.NoHttp;
 import com.yanzhenjie.nohttp.RequestMethod;
 import com.yanzhenjie.nohttp.rest.Request;
 import com.yanzhenjie.nohttp.rest.Response;
+import com.youth.banner.transformer.DefaultTransformer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,6 +47,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import fr.castorflex.android.verticalviewpager.VerticalViewPager;
 
 public class InviteActivity extends BaseActivity {
     private static final int WHAT_INVITE_RULE = 0;
@@ -56,26 +56,9 @@ public class InviteActivity extends BaseActivity {
 
     @BindView(R.id.tb_login_title)
     TitleBarView tbLoginTitle;
-    @BindView(R.id.sv)
-    ScrollzfView sv;
-    @BindView(R.id.tv_invite_rule_one)
-    TextView tvInviteRuleOne;
-    @BindView(R.id.tv_invite_rule_two)
-    TextView tvInviteRuleTwo;
-    @BindView(R.id.tv_invite_rule_three)
-    TextView tvInviteRuleThree;
-    @BindView(R.id.tv_invite_rule_four)
-    TextView tvInviteRuleFour;
-    @BindView(R.id.iv_invite_share)
-    ImageView ivInviteShare;
-    @BindView(R.id.tv_invite_detail_btn)
-    TextView tvInviteDetailBtn;
-    @BindView(R.id.lv_invite)
-    ListView lvInvite;
-    @BindView(R.id.srl_my_invite)
-    SmartRefreshLayout srlMyInvite;
-    @BindView(R.id.tv_no_list)
-    TextView tvNoList;
+    @BindView(R.id.vvp)
+    VerticalViewPager viewPager;
+
     private String userId;
     private List<InviteRuleResponse.DataBean.ComplexBean> complex;
     private List<InviteRuleResponse.DataBean.SimpleBean> simple;
@@ -86,6 +69,10 @@ public class InviteActivity extends BaseActivity {
     private int page = 1;
     private static int pageSize = 10;
     private int pages = 10;
+    private View view1;
+    private View view2;
+    private ViewHolder1 holder1;
+    private ViewHolder2 holder2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -113,18 +100,24 @@ public class InviteActivity extends BaseActivity {
 //            }
 //        });
 
+        view1 = View.inflate(this, R.layout.activity_invite_1, null);
+        holder1 = new ViewHolder1(view1);
+        view2 = View.inflate(this, R.layout.activity_invite_2, null);
+        holder2 = new ViewHolder2(view2);
+        initVVP();
 
 
-        srlMyInvite.setRefreshHeader(new MaterialHeader(this));
-        srlMyInvite.setRefreshFooter(new ClassicsFooter(this));
-        srlMyInvite.setOnRefreshListener(new OnRefreshListener() {
-            @Override
-            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
-                page = 1;
-                getInviteList(page, false);
-            }
-        });
-        srlMyInvite.setOnLoadMoreListener(new OnLoadMoreListener() {
+//        srlMyInvite.setRefreshHeader(new MaterialHeader(this));
+        holder2.srlMyInvite.setEnableRefresh(false);
+        holder2.srlMyInvite.setRefreshFooter(new ClassicsFooter(this));
+//        srlMyInvite.setOnRefreshListener(new OnRefreshListener() {
+//            @Override
+//            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+//                page = 1;
+//                getInviteList(page, false);
+//            }
+//        });
+        holder2.srlMyInvite.setOnLoadMoreListener(new OnLoadMoreListener() {
             @Override
             public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
                 getInviteList(page, false);
@@ -133,44 +126,40 @@ public class InviteActivity extends BaseActivity {
 
         data = new ArrayList<>();
         adapter = new InviteListAdapter(this, data);
-        lvInvite.setDividerHeight(0);
-        lvInvite.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        lvInvite.setSelector(R.color.transparent);
-        lvInvite.setAdapter(adapter);
+        holder2.lvInvite.setDividerHeight(0);
+        holder2.lvInvite.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        holder2.lvInvite.setSelector(R.color.transparent);
+        holder2.lvInvite.setAdapter(adapter);
     }
 
-    @OnClick({R.id.tv_invite_detail_btn, R.id.iv_invite_share})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.tv_invite_detail_btn:
-//                List<InviteListResponse.DataBean.ListBean> data1 = new ArrayList<>();
-//                data1.add(new InviteListResponse.DataBean.ListBean("1","111"));
-//                data1.add(new InviteListResponse.DataBean.ListBean("2","222"));
-//                data1.add(new InviteListResponse.DataBean.ListBean("3","333"));
-//                WheelViewDialog dialog = new WheelViewDialog(this, data1);
-//                dialog.setListener(new BaseDialog.DialogListener() {
-//                    @Override
-//                    public void onClickType(int type, KeyValue bean) {
-//
-//                    }
-//                });
-                String inviteDetail = StringUtil.EMPTY;
-                for (int i = 0; i < complex.size(); i++) {
-                    inviteDetail = inviteDetail + complex.get(i).getLine() + "\n\n";
-                }
-                InviteDetailDialog inviteDetailDialog = new InviteDetailDialog(InviteActivity.this, inviteDetail);
-                inviteDetailDialog.show();
-                break;
-            case R.id.iv_invite_share:
-                UMWeb web = new UMWeb(shareInfo.getShareUrl());
-                web.setTitle(shareInfo.getTitle());//标题
-                web.setThumb(new UMImage(this, shareInfo.getImageUrl()));  //缩略图
-                web.setDescription(shareInfo.getContext());//描述
-                new ShareAction(InviteActivity.this).withMedia(web).setDisplayList(SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE)
-                        .setCallback(shareListener).open();
-                break;
-        }
+    private void initVVP() {
+        viewPager.setAdapter(new PagerAdapter() {
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == arg1;
+            }
+
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                View v = position == 1 ? view2 : view1;
+                container.addView(v);
+                return v;
+            }
+        });
+        viewPager.setPageTransformer(false, new DefaultTransformer());
+
+//viewPager.setPageTransformer(false, new ZoomOutTransformer());
+//viewPager.setPageTransformer(true, new StackTransformer());
     }
+
+
 
     //获取邀请规则
     public void getInviteRule() {
@@ -220,10 +209,10 @@ public class InviteActivity extends BaseActivity {
                     if (inviteRuleResponse.getBackStatus() == 0) {
                         complex = inviteRuleResponse.getData().getComplex();
                         simple = inviteRuleResponse.getData().getSimple();
-                        tvInviteRuleOne.setText(simple.get(0).getLine());
-                        tvInviteRuleTwo.setText(simple.get(1).getLine());
-                        tvInviteRuleThree.setText(simple.get(2).getLine());
-                        tvInviteRuleFour.setText(simple.get(3).getLine());
+                        holder1.tvInviteRuleOne.setText(simple.get(0).getLine());
+                        holder1.tvInviteRuleTwo.setText(simple.get(1).getLine());
+                        holder1.tvInviteRuleThree.setText(simple.get(2).getLine());
+                        holder1.tvInviteRuleFour.setText(simple.get(3).getLine());
                     } else {
                         ToastUtil.show(inviteRuleResponse.getMessage());
                     }
@@ -248,8 +237,8 @@ public class InviteActivity extends BaseActivity {
                             adapter.notifyDataSetChanged();
 //                            listPayList.setSelection(0);
                         } else {
-                            tvNoList.setVisibility(View.VISIBLE);
-                            lvInvite.setVisibility(View.GONE);
+                            holder2.tvNoList.setVisibility(View.VISIBLE);
+                            holder2.lvInvite.setVisibility(View.GONE);
                         }
                     } else {
                         ToastUtil.show(inviteListResponse.getMessage());
@@ -257,8 +246,8 @@ public class InviteActivity extends BaseActivity {
                     if (page > pages) {
                         hasMoreData = true;
                     }
-                    srlMyInvite.finishRefresh(1000, true);
-                    srlMyInvite.finishLoadMore(1000, true, hasMoreData);
+//                    srlMyInvite.finishRefresh(1000, true);
+                    holder2.srlMyInvite.finishLoadMore(1000, true, hasMoreData);
                     break;
                 case WHAT_SHARE:
                     String shareJson = response.get();
@@ -276,8 +265,8 @@ public class InviteActivity extends BaseActivity {
         @Override
         public void onFailed(int what, Response<String> response) {
             String json = response.get();
-            srlMyInvite.finishRefresh(1000, true);
-            srlMyInvite.finishLoadMore(1000, true, hasMoreData);
+//            srlMyInvite.finishRefresh(1000, true);
+//            srlMyInvite.finishLoadMore(1000, true, hasMoreData);
             DialogUtil.dismissLoading();
             LogUtils.d("SJHttp", json);
         }
@@ -320,4 +309,70 @@ public class InviteActivity extends BaseActivity {
 
         }
     };
+
+    public class ViewHolder1 {
+        @BindView(R.id.tv_invite_rule_one)
+        TextView tvInviteRuleOne;
+        @BindView(R.id.tv_invite_rule_two)
+        TextView tvInviteRuleTwo;
+        @BindView(R.id.tv_invite_rule_three)
+        TextView tvInviteRuleThree;
+        @BindView(R.id.tv_invite_rule_four)
+        TextView tvInviteRuleFour;
+        @BindView(R.id.tv_invite_detail_btn)
+        TextView tvInviteDetailBtn;
+        @BindView(R.id.iv_invite_share)
+        ImageView ivInviteShare;
+
+        ViewHolder1(View view) {
+            ButterKnife.bind(this, view);
+        }
+
+
+        @OnClick({R.id.tv_invite_detail_btn, R.id.iv_invite_share})
+        public void onViewClicked(View view) {
+            switch (view.getId()) {
+                case R.id.tv_invite_detail_btn:
+//                List<InviteListResponse.DataBean.ListBean> data1 = new ArrayList<>();
+//                data1.add(new InviteListResponse.DataBean.ListBean("1","111"));
+//                data1.add(new InviteListResponse.DataBean.ListBean("2","222"));
+//                data1.add(new InviteListResponse.DataBean.ListBean("3","333"));
+//                WheelViewDialog dialog = new WheelViewDialog(this, data1);
+//                dialog.setListener(new BaseDialog.DialogListener() {
+//                    @Override
+//                    public void onClickType(int type, KeyValue bean) {
+//
+//                    }
+//                });
+                    String inviteDetail = StringUtil.EMPTY;
+                    for (int i = 0; i < complex.size(); i++) {
+                        inviteDetail = inviteDetail + complex.get(i).getLine() + "\n\n";
+                    }
+                    InviteDetailDialog inviteDetailDialog = new InviteDetailDialog(InviteActivity.this, inviteDetail);
+                    inviteDetailDialog.show();
+                    break;
+                case R.id.iv_invite_share:
+                    UMWeb web = new UMWeb(shareInfo.getShareUrl());
+                    web.setTitle(shareInfo.getTitle());//标题
+                    web.setThumb(new UMImage(InviteActivity.this, shareInfo.getImageUrl()));  //缩略图
+                    web.setDescription(shareInfo.getContext());//描述
+                    new ShareAction(InviteActivity.this).withMedia(web).setDisplayList(SHARE_MEDIA.WEIXIN, SHARE_MEDIA.WEIXIN_CIRCLE)
+                            .setCallback(shareListener).open();
+                    break;
+            }
+        }
+    }
+
+    public class ViewHolder2 {
+        @BindView(R.id.lv_invite)
+        MyListView lvInvite;
+        @BindView(R.id.tv_no_list)
+        TextView tvNoList;
+        @BindView(R.id.srl_my_invite)
+        SmartRefreshLayout srlMyInvite;
+
+        ViewHolder2(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
 }
--- app/src/main/java/com/sujin/sjpay/view/MyListView.java
+++ app/src/main/java/com/sujin/sjpay/view/MyListView.java
@@ -2,9 +2,13 @@ package com.sujin.sjpay.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.ListView;
 
 public class MyListView extends ListView {
+
+    private float oldY,newY;
+
     public MyListView(Context context) {
         super(context);
     }
@@ -13,10 +17,43 @@ public class MyListView extends ListView {
         super(context, attrs);
     }
 
+//    @Override
+//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+//        //测量的大小由一个32位的数字表示，前两位表示测量模式，后30位表示大小，这里需要右移两位才能拿到测量的大小
+//        int heightSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+//        super.onMeasure(widthMeasureSpec, heightSpec);
+//    }
+
+
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        //测量的大小由一个32位的数字表示，前两位表示测量模式，后30位表示大小，这里需要右移两位才能拿到测量的大小
-        int heightSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
-        super.onMeasure(widthMeasureSpec, heightSpec);
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        if (getFirstVisiblePosition() == 0 && getChildAt(0).getTop() == 0) {//到头部了
+            getParent().requestDisallowInterceptTouchEvent(false);//放行
+        } else {
+            getParent().requestDisallowInterceptTouchEvent(true);//拦截
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                oldY = ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                newY = ev.getY();
+                if(newY>oldY && getFirstVisiblePosition() == 0 && getChildAt(0).getTop() == 0){
+                    return true;
+                }
+
+                oldY = newY;
+                break;
+        }
+
+
+        return super.onTouchEvent(ev);
     }
 }
